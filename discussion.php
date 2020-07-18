939acdbf67f719c33dc0c4fde118f68a390d248c
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-07-18 18:21:30 +0200

    Add files via upload

--- discussion.php
+++ discussion.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="style.css">
@@ -11,41 +11,42 @@
 
         <!-- Afficher le contenu d'un document CSV sous forme de discussion interactive et sécurisée. -->
 
-        <?php
-        // Ouvrir le document CSV (lecture).
-        $file = fopen("discussion.csv", "r");
+        
+        <!-- Ouvrir le document CSV (lecture). -->
+        <?php $file = fopen("discussion.csv", "r"); ?>
 
-        // Pour chaque ligne :
-        while(!feof($file)) {
+        <!-- Pour chaque ligne : -->
+        <?php while(!feof($file)) {
             // Ligne en cours.
-            $data = fgetcsv($file);
+            $data = fgetcsv($file);?>
 
-            // Définir des variables aux nom précis.
-            //$message = array(
+            <!-- Définir des variables aux nom précis.-->
+            <?php //$message = array(
                 //'author' => $data[0],
                 //'theme' => $data[1],
                 //'group' => $data[2],
                 //'text' => $data[3],
             //);
 
-            //print_r($message);
             // Notice: Trying to access array offset on value of type bool 
-            // in /opt/lampp/htdocs/test_t/discussion.php on line 24, 25, 26, 27
+            // in /opt/lampp/htdocs/test_t/discussion.php on line 27, 28, 29, 30
+            ?> 
             
-            // Vérifier les données reçues.
-            if (isset($data[0]) && isset($data[1]) && isset($data[2]) && isset($data[3])) {
-        ?>      <!-- Selon le groupe Private ou Public la couleur du cadre change. -->
-                <div class=<?php echo '"' . strip_tags($data[2]) . '"'; ?> >
+            <!-- Vérifier les données reçues. -->
+            <?php if (isset($data[0]) && isset($data[1]) && isset($data[2]) && isset($data[3])) { ?>
+            <!-- Selon le groupe private ou public la couleur du cadre change. -->
+                <div class=<?php echo '"' . strtolower(strip_tags($data[2])) . '"'; ?> >
                     <p>
                         <!-- Afficher le contenu : ! htmlspecialchars() ; nl2br()
                         Discussion créée par Auteur (strong) -->
-                        Discussion créée par <strong><?php echo htmlspecialchars($data[0]); ?></strong><br/>
+                        Discussion créée par <strong><?php echo nl2br(htmlspecialchars($data[0])); ?></strong><br/>
 
                         <!-- (Sur le thème : Thème(strong)) -->
                         (Sur le thème : <strong><?php echo htmlspecialchars($data[1]); ?></strong>)<br/>
 
                         <!-- Dans le groupe Groupe -->
                         Dans le groupe <?php echo htmlspecialchars($data[2]); ?>
+
                     </p>
 
                     <p>
@@ -53,23 +54,23 @@
                         <em>" <?php echo nl2br(htmlspecialchars($data[3])); ?> "</em>
                     </p>
                 </div><!-- class="GROUP" -->
-                <?php
-            }
-        }
+                
+            <?php }
+        } ?>
         
-        // Fermer le document CSV.
-        fclose($file);
+        <!-- Fermer le document CSV. -->
+        <?php fclose($file); ?>
 
-        if (isset($_GET['envoi'])) {
-            //Le message est affiché quelque soit la valeur de $_GET['envoi'].
-            ?>
-            <p id=echec>
+        <?php if (isset($_GET['sent'])) { ?>
+            <!-- Le message est affiché quelque soit la valeur de $_GET['envoi']. -->
+            
+            <p id=fail>
                 Un ou plusieurs champs sont vides. L'envoi a échoué.
             </p>
-            <?php
-        }
+            
+        <?php } ?>
 
-            ?>
+            
         <!-- Ajouter un commentaire. -->
         <div id="formulaire">
             <p>
@@ -80,7 +81,7 @@
             <form action="post_discussion.php" method="POST">
                 <p>
                     <!-- Nom d'utilisateur (texte court) -->
-                    <label>Nom d'utilisateur<input action="text" name="nom"></label>
+                    <label>Nom d'utilisateur<input type="text" name="author"></label>
 
                     <!-- Thème (menu déroulant) --> 
                     <select id="theme" name="theme">
@@ -90,16 +91,16 @@
                     </select> 
 
                     <!-- Groupe (radiobutton) -->
-                    <input type="radio" id="public" name="groupe" value="Public">
+                    <input type="radio" id="public" name="group" value="Public">
                     <label for="public">Public</label>
-                    <input type="radio" id="private" name="groupe" value="Private">
+                    <input type="radio" id="private" name="group" value="Private">
                     <label for="private">Private</label>
                 </p>
 
                 <p>
                     <!-- Message (Texte long) -->
                     <label>Message<br/></label>
-                    <textarea name="message" maxlength="300"></textarea>
+                    <textarea name="text" maxlength="300"></textarea>
                 </p>
 
                 <!-- Compteur de carractères -->
--- post_discussion.php
+++ post_discussion.php
@@ -5,23 +5,23 @@
         <meta charset="utf-8">
     </head>
 
-
     <body>
 
         <?php
 
         // Vérifier la validité (aucun champs vides) :
-        if (empty($_POST['nom']) || empty($_POST['theme']) || empty($_POST['groupe']) || empty($_POST['message'])) {
+        if (empty($_POST['author']) || empty($_POST['theme']) || empty($_POST['group']) || empty($_POST['text'])) {
             // Envoi statut erreur.
-            header('Location: discussion.php?envoi=false');
+            header('Location: discussion.php?sent=false');
         }
 
         else {
+            // Le fichier CSV doit être écrit sous Linux pour que ce code fonctionne sur un serveur Linux.
+
             // Ouvrir le document CSV (écriture);
-            $file = fopen("discussion.csv","a+");
+            $file = fopen('discussion.csv','a');
+          
             // Enregistrer;
-            $void_line = array('', ''); // Les deux lignes que je souhaite améliorer ici
-            fputcsv($file, $void_line); // et ici.
             fputcsv($file, $_POST);
         
             // Fermer le document CSV;
